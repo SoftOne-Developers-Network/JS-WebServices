fb7e31af676ee15ad0d0a8dd8a3d2d6cc0320660
Giannis Giorgoulakis <giannis@giorgoulakis.com>
2017-01-30 15:13:57 +0200

    Update connector.js

--- connector.js
+++ connector.js
@@ -21,7 +21,8 @@ function sql2json(sql)
   var ds = X.GETSQLDATASET(sql, null);
 	if (ds.RECORDCOUNT>0){
 		resp.success=true;
-		resp.data = eval(ds.JSON); 
+		//resp.data = eval(ds.JSON); //delays and wasting memory
+		resp.data = ds.JSON; 
 		return resp;
 	}else{
 		resp.success=false;
